55b7906bef877dcdb6fc33740d1c1736995186f2
fkadeal <fkadeal@github.com>
2021-10-03 03:58:28 -0700

    add localhost to work localy

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -21,7 +21,7 @@ $(document).ready(function () {
 
   $.ajax({
     type: 'GET',
-    url: 'http://0.0.0.0:5001/api/v1/status/',
+    url: 'http://localhost:5001/api/v1/status/',
     success: function (data) {
       if (data.status === 'OK') {
         console.log(data.status);
